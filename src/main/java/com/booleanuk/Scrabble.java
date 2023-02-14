d0394bb766603c5cd3f5e780e3bb00c51e43ea06
Nathan King <nathanking3590@gmail.com>
2023-02-14 15:05:18 +0000

    add solution

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,5 +1,30 @@
 package com.booleanuk;
 
-public class Scrabble {
+import java.util.Map;
 
+public class Scrabble {
+    private final String word;
+    private final Map<Character, Integer> letterScores;
+
+    public Scrabble(String word) {
+        this.word = word;
+        this.letterScores = Alphabet.getLetterScores();
+    }
+
+    public int score() {
+        int total = 0;
+
+        if (this.word.length() == 0) {
+            return total;
+        }
+
+        String lower = this.word.trim().toLowerCase();
+
+        for (char ch : lower.toCharArray()) {
+            int score = this.letterScores.get(ch);
+            total += score;
+        }
+
+        return total;
+    }
 }
