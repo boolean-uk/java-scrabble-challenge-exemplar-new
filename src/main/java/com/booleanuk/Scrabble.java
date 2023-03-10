dbef25f41b91ccc2d8cf45a7d715088d6867cb2b
Dave Ames <david.john.ames@gmail.com>
2023-03-10 17:59:05 +0000

    Solution for the main exercise part of the Scrabble Challenge.

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,5 +1,53 @@
 package com.booleanuk;
 
-public class Scrabble {
+import java.util.HashMap;
 
+public class Scrabble {
+    HashMap<Character, Integer> letterScore = new HashMap<>();
+    String word;
+    public Scrabble(String word) {
+        this.word = word.toUpperCase();
+        this.buildScores();
+    }
+    
+    public void buildScores() {
+        letterScore.put('A', 1);
+        letterScore.put('B', 3);
+        letterScore.put('C', 3);
+        letterScore.put('D', 2);
+        letterScore.put('E', 1);
+        letterScore.put('F', 4);
+        letterScore.put('G', 2);
+        letterScore.put('H', 4);
+        letterScore.put('I', 1);
+        letterScore.put('J', 8);
+        letterScore.put('K', 5);
+        letterScore.put('L', 1);
+        letterScore.put('M', 3);
+        letterScore.put('N', 1);
+        letterScore.put('O', 1);
+        letterScore.put('P', 3);
+        letterScore.put('Q', 10);
+        letterScore.put('R', 1);
+        letterScore.put('S', 1);
+        letterScore.put('T', 1);
+        letterScore.put('U', 1);
+        letterScore.put('V', 4);
+        letterScore.put('W', 4);
+        letterScore.put('X', 8);
+        letterScore.put('Y', 4);
+        letterScore.put('Z', 10);
+    }
+
+    public int score() {
+        int totalScore = 0;
+        for (char ch : this.word.toCharArray()) {
+            totalScore += getLetterScore(ch);
+        }
+        return totalScore;
+    }
+
+    public int getLetterScore(char letter) {
+        return this.letterScore.getOrDefault(letter, 0);
+    }
 }
