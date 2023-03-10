a4e4a7113841a11a555c38bee27a831032f94e6b
Dave Ames <david.john.ames@gmail.com>
2023-03-10 18:02:05 +0000

    Tests added for the double and triple word and letter scores including nested ones.

--- src/test/java/com/booleanuk/ScrabbleTest.java
+++ src/test/java/com/booleanuk/ScrabbleTest.java
@@ -45,4 +45,51 @@ class ScrabbleTest {
         Scrabble scrabble = new Scrabble("OXyPHEnBUTaZoNE");
         Assertions.assertEquals(41, scrabble.score());
     }
+
+    @Test
+    public void doubleLetterScoreScores16() {
+        // t scores 1, w scores 4, e scores 1, l scores 1, v scores 4 doubled scores 8, e scores 1
+        // 1+4+1+1+8+1= 16
+        Scrabble scrabble = new Scrabble("twel[v]e");
+        Assertions.assertEquals(16, scrabble.score());
+    }
+    @Test
+    public void tripleLetterScoreScores20() {
+        // t scores 1, w scores 4, e scores 1, l scores 1, v scores 4 tripled scores `1, e scores 1
+        // 1+4+1+1+12+1=20
+        Scrabble scrabble = new Scrabble("twel{v}e");
+        Assertions.assertEquals(20, scrabble.score());
+    }
+
+    @Test
+    public void doubleWordScoreScores22() {
+        // j scores 8, o scores 1, g scores 2, 8+1+2 = 11
+        // Double word score makes 22
+        Scrabble scrabble = new Scrabble("[jog]");
+        Assertions.assertEquals(22, scrabble.score());
+    }
+
+    @Test
+    public void tripleWordScoreScores15() {
+        // d scores 2, o scores 1, g scores 2, 2+1+2 = 5
+        // Triple word score makes 15
+        Scrabble scrabble = new Scrabble("{dog}");
+        Assertions.assertEquals(15, scrabble.score());
+    }
+
+    @Test
+    public void nestedTripleLetterInDoubleWordScores22() {
+        // f scores 4, o scores 1, g scores 2 tripled = 6, 4+1+6 = 11
+        // Double word score gives 22
+        Scrabble scrabble = new Scrabble("[fo{g}]");
+        Assertions.assertEquals(22, scrabble.score());
+    }
+
+    @Test
+    public void nestedDoubleLetterInTripleWordScores33() {
+        // f scores 4 doubled = 8, o scores 1, g scores 2, 8+1+2 = 11
+        // Triple word score gives 33
+        Scrabble scrabble = new Scrabble("{[f]og}");
+        Assertions.assertEquals(33, scrabble.score());
+    }
 }
